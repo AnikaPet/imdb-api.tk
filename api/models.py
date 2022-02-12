27c22f7955e2a255640a9a1f0fcceae503dea9d2
AnikaPet <anika.petrovic1999@gmail.com>
2022-02-12 18:22:38 +0100

    Add ratings as choices.

--- api/models.py
+++ api/models.py
@@ -15,7 +15,6 @@ class Country(models.Model):
 
 class Company(models.Model):
     name = models.CharField(max_length=200)
-    established = models.CharField(max_length=4)
     country = models.ForeignKey(Country, related_name="company_country", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
@@ -77,8 +76,21 @@ class Profile(models.Model):
     def __str__(self):
         return self.name
 
+RATINGS = (
+    (1,'1'),
+    (2,'2'),
+    (3,'3'),
+    (4,'4'),
+    (5,'5'),
+    (6,'6'),
+    (7,'7'),
+    (8,'8'),
+    (9,'9'),
+    (10,'10')
+)
+
 class Review(models.Model):
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
     movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
     comment = models.TextField(max_length=300)
-    rate = models.IntegerField()
+    rate = models.IntegerField(choices=RATINGS)
