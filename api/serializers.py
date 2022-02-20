403d57d8f71a9d9a93e99f70eb81d674c174069d
AnikaPet <anika.petrovic1999@gmail.com>
2022-02-20 19:50:43 +0100

    Allow registration for users.

--- api/serializers.py
+++ api/serializers.py
@@ -7,9 +7,27 @@ from django.contrib.auth.password_validation import validate_password
 from .models import Profile,Movie,Cast,Company,Country,Language,Genre,Actor,Review
 
 class RegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        exclude = []
+        fields = ('username', 'password', 'password2', 'email')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])   
+        return user
 
 
 class ProfileSerializer(serializers.ModelSerializer):
