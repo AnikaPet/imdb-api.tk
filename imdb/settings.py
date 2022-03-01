2a3b6a6c2ac631a6a0d14454e610b8ac32850294
AnikaPet <anika.petrovic1999@gmail.com>
2022-03-01 18:13:50 +0100

    Add allowed hosts

--- imdb/settings.py
+++ imdb/settings.py
@@ -148,6 +148,8 @@ STATIC_ROOT = BASE_DIR / 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+ALLOWED_HOSTS = ['*']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
