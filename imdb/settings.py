6de154c73cd1e317ea864dc508a4591e18731484
anikapet <81329372+AnikaPet@users.noreply.github.com>
2022-03-01 18:36:08 +0100

    Update settings.py

--- imdb/settings.py
+++ imdb/settings.py
@@ -102,7 +102,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        #'rest_framework.authentication.BasicAuthentication'
     ),
 
 }
